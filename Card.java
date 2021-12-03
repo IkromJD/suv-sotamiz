c502473dd0e1f0009e5a051b2340fe4a8faa4ee1
IkromJD <95452344+IkromJD@users.noreply.github.com>
2021-12-04 01:54:41 +0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,26 @@
+public class Card {
+
+    private int cardId;
+    private double credit;
+
+    public Card(int cardId, double credit) {
+        this.cardId = cardId;
+        this.credit = credit;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void addCredit(double credit) {
+        this.credit += credit;
+    }
+
+    public void sell(double price) {
+        this.credit -= price;
+    }
+}
